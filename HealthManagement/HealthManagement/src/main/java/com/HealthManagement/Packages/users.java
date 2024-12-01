91a839c853c916a84cf2a1558000f5fb5c3f4e55
userscreatingbr <msivaramaiah@gmail.com>
2024-12-01 15:00:44 +0530

    users class added

---
+++ HealthManagement/HealthManagement/src/main/java/com/HealthManagement/Packages/users.java
@@ -0,0 +1,17 @@
+package com.HealthManagement.Packages;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class users {
+    private long id;
+    private String name;
+}
